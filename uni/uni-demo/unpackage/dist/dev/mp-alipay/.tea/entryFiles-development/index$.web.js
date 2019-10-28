841387da5ba9b09363b4bec2c38ce84d218b6855
Fancymeng <793514513@qq.com>
2019-10-28 17:41:29 +0800

    又做了一点

--- uni/uni-demo/unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
+++ uni/uni-demo/unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
@@ -1,5 +1,7 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
+require('../../components/xyz-tab/xyz-tab?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/web/common-webview/common-webview?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/shop/shop?hash=72c9638772422d5c06838a1a5918a07a238db907');
--- uni/uni-demo/unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.worker.js
+++ uni/uni-demo/unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.worker.js
@@ -22,8 +22,10 @@ self.requirePlugin = AFAppX.requirePlugin;
 
 function success() {
 require('../../app');
+require('../../components/xyz-tab/xyz-tab?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/web/common-webview/common-webview?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/shop/shop?hash=72c9638772422d5c06838a1a5918a07a238db907');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
